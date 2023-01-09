3d3431f8fc816b852f61fa85b02ce622d127d8ed
jahadtariq <muhammadjahadtariq@gmail.com>
2023-01-09 06:01:27 +0500

    initiated components

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Billing from './Billing';
+import Business from './Business';
+import Button from './Button';
+import CardDeal from './CardDeal';
+import CTA from './CTA';
+import Clients from './Clients';
+import feedbackCard from './FeedbackCard';
+import Footer from './Footer';
+import GetStarted from './GetStarted';
+import Stats from './Stats';
+import Testimonials from './Testimonails';
+
+export {
+    Navbar,
+    Hero,
+    Billing,
+    Business,
+    Button,
+    CardDeal,
+    CTA,
+    feedbackCard,
+    Footer,
+    GetStarted,
+    Stats,
+    Testimonials,
+    Clients
+}
